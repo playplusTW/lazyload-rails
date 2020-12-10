c4ea24b5c552c4e98736209f94f4700217387b5d
chang <ycw99@hotmail.com>
2020-12-10 14:16:28 +0800

    Update lazyload-rails.rb

--- lib/lazyload-rails.rb
+++ lib/lazyload-rails.rb
@@ -51,7 +51,7 @@ ActionView::Helpers::AssetTagHelper.module_eval do
   def to_lazy(image_html)
     img = Nokogiri::HTML::DocumentFragment.parse(image_html).at_css("img")
 
-    img["data-original"] = img["src"]
+    img["data-src"] = img["src"]
     img["src"] = Lazyload::Rails.configuration.placeholder
 
     img.to_s.html_safe
